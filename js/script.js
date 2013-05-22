ff2438d4561f599f8f1c85fcdaea08ec6ed05b30
Splintex <godun.yevgen@gmail.com>
2013-05-23 00:20:58 +0300

    DONE!

--- js/script.js
+++ js/script.js
@@ -995,3 +995,16 @@ jQuery(document).ready(function($) {
     });    
     SiteModel.init();
 });
+
+jQuery(document).ready(function($){
+var slideshows = $('.cycle-slideshow').on('cycle-next cycle-prev', function(e, opts) {
+    // advance the other slideshow
+    slideshows.not(this).cycle('goto', opts.currSlide);
+});
+
+$('#cycle-2 .cycle-slide').click(function(){
+    var index = $('#cycle-2').data('cycle.API').getSlideIndex(this);
+    slideshows.cycle('goto', index);
+});
+
+});
